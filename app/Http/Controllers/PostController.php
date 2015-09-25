37f81127826b6d70eadf88ef96fc4ee9ddfe076a
alnutile <alfrednutile@gmail.com>
2015-09-24 22:05:24 -0400

    Pushing up work

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,115 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		$posts = Post::all();
+
+		return view('posts.index', compact('posts'));
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return view('posts.create');
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param Request $request
+	 * @return Response
+	 */
+	public function store(Request $request)
+	{
+		$this->validate($request, ['title' => 'required|min:3', 'body' => 'required|max:250']);
+
+		$post = new Post();
+
+		$post->title = $request->input("title");
+        $post->body = $request->input("body");
+        $post->user_id = (Auth::guest()) ? 0 : 1;
+
+		$post->save();
+
+		return redirect()->route('posts.index')->with('message', 'Post created successfully.');
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		$post = Post::findOrFail($id);
+
+		return view('posts.show', compact('post'));
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		$post = Post::findOrFail($id);
+
+		return view('posts.edit', compact('post'));
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @param Request $request
+	 * @return Response
+	 */
+	public function update(Request $request, $id)
+	{
+		$post = Post::findOrFail($id);
+
+		$post->title = $request->input("title");
+        $post->body = $request->input("body");
+        $post->user_id = $request->input("user_id");
+
+		$post->save();
+
+		return redirect()->route('posts.index')->with('message', 'Item updated successfully.');
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		$post = Post::findOrFail($id);
+		$post->delete();
+
+		return redirect()->route('posts.index')->with('message', 'Item deleted successfully.');
+	}
+
+}
